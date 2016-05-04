11e695e90972c37a6572a2919b3305e72c3ddb65
David Asabina <david@supr.nu>
2016-05-03 20:44:34 -0400

    Added NavBarButtonPlus@3x

--- index.ios.js
+++ index.ios.js
@@ -7,47 +7,75 @@
 import React, {
   AppRegistry,
   Component,
+  NavigatorIOS,
   StyleSheet,
   Text,
+	ListView,
+  ScrollView,
   View
 } from 'react-native';
 
-class NotitieBlok extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
+import DemoSimpleList from './components/DemoSimpleList';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
 });
+
+class Main extends Component {
+	constructor(props) {
+		super(props)
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+		this.state = {
+			dataSource: ds.cloneWithRows(['morgan', 'barclays', 'rabo', 'roth', 'rocker', 'london', 'new york', 'carnegie', 'lehman', 'v/d bilt', 'chase', 'hartman', 'stilman'])
+		}
+		console.log(props)
+		/*
+		props.navigator.push({
+          component: Add,
+          title: 'Nieuwe Boeking',
+					leftButtonTitle: 'Overzicht',
+					onLeftButtonPress: () => {props.navigator.pop()}
+    })
+*/
+	}
+  render() {
+    return (
+      <ScrollView style={styles.list}>
+				<DemoSimpleList dataSource={this.state.dataSource} />
+      </ScrollView>
+    );
+  }
+}
+
+class Add extends Component {
+  render() {
+    return (
+      <ScrollView style={styles.list}>
+        <Text style={styles.welcome}>
+          Add something here
+        </Text>
+      </ScrollView>
+    );
+  }
+}
+
+class NotitieBlok extends Component {
+  render() {
+    return (
+      <NavigatorIOS
+				style={styles.container}
+        initialRoute={{
+          component: Main,
+          title: 'Boekingen',
+					rightButtonIcon: require('image!NavBarButtonPlus'),
+					onLeftButtonPress: () => {console.log('pressed')},
+        }}
+        itemWrapperStyle={styles.itemWrapper}
+      />
+    );
+  }
+}
 
 AppRegistry.registerComponent('NotitieBlok', () => NotitieBlok);
