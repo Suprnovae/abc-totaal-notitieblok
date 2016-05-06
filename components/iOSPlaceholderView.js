0e0f57a479358c215ed5bc8a67ddac24d9801c8c
Alina Chernenko <ailisenok17@yahoo.com>
2016-05-06 13:21:13 +0300

    - text truncation done - styles updates

--- components/iOSPlaceholderView.js
+++ components/iOSPlaceholderView.js
@@ -17,12 +17,26 @@ import styles from '../styles/Initial';
 export default class iOSPlaceholderView extends Component {
 
   constructor(props) {
-        super(props)
-        const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-        this.state = {
-        dataSource: ds.cloneWithRows(['morgan', 'barclays', 'rabo', 'roth', 'rocker', 'london', 'new york', 'carnegie', 'lehman', 'v/d bilt', 'chase', 'hartman', 'stilman'])
-        }
-        console.log(props)
+    super(props);
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      dataSource: ds.cloneWithRows([{
+        "price": -4.0,
+        "description": "1x seat for e-mail service",
+        "datetime": "2016/05/03",
+        "attachment": null,
+        "category": "internet services"
+      }, {
+        "price": 1229,
+        "description": "laptop",
+        "datetime": "2016/05/03",
+        "attachment": null,
+        "category": "tools"
+      }
+])
+    };
+        
+    console.log(props);
         /*
          props.navigator.push({
          component: Add,
@@ -31,12 +45,12 @@ export default class iOSPlaceholderView extends Component {
          onLeftButtonPress: () => {props.navigator.pop()}
          })
          */
-    }
-    render() {
-        return (
+  }
+  render() {
+    return (
                 <ScrollView style={styles.list}>
                 <DemoSimpleList dataSource={this.state.dataSource} />
                 </ScrollView>
-                );
-    }
+  );
+  }
   }
