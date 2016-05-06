9e1c3ad870c1c48273a2e7c6f5b3621fabd20841
Alina Chernenko <ailisenok17@yahoo.com>
2016-05-06 10:56:19 +0300

    - iOSPlaceholderView component was created

--- index.ios.js
+++ index.ios.js
@@ -1,3 +1,4 @@
+
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
@@ -8,202 +9,57 @@
 
 var React = require('react-native');
 
-var _     = require('lodash');
-
-var Records = require('./data/records.js');
-
-//debugger;
-
 var {
-  AppRegistry,
-  Component,
-  NavigatorIOS,
-  StyleSheet,
-  Text,
-  View,
-ListView,
+    AppRegistry,
+    Component,
+    StyleSheet,
+    Text,
+    View,
+    ListView,
     RecyclerViewBackedScrollView,
+    NavigatorIOS,
+    ScrollView
 } = React;
 
+import DemoSimpleList from './components/DemoSimpleList';
+import iOSPlaceholderView from './components/iOSPlaceholderView';
 
 //state, props
-var export default class BasicRecordListElement extends Component({
-//var NotitieBlok  = React.createClass({
-                                     
-                                     getInitialState: function(){
-                                     return {
-                                     dataSource: new ListView.DataSource({rowHasChanged: (row1, row2) => row1 !== row2})
-                                     }
-                                     },
-                                     
-    
-                                     componentDidMount: function(){
-                                     this.setState({
-                                                   dataSource: this.state.dataSource.cloneWithRows(Records.records),
-                                     });
-                                                                  },
-    render: function() {
-                                     //debugger;
-    return (
-            
-            <ListView
-            dataSource = {this.state.dataSource}
-            renderRow = {this.renderRecord}
-            renderScrollComponent={props => <RecyclerViewBackedScrollView {...props} />}
-            renderSeparator={(sectionID, rowID) => <View key={`${sectionID}-${rowID}`} style={styles.separator} />}
-            style = {styles.ListView}
-            ></ListView>
-            
-//      <View style={styles.container}>
-//        <Text style={styles.welcome}>
-//          Welcome to React Native!
-//        </Text>
-//      </View>
-    );
-  },
-                                     
-                                     renderRecord: function(record){
-                                     return(
-                                     <View>
-                                            
-                                            <Text style = {styles.leftColor}>
-                                            </Text>
-                                            <Text style = {styles.descriptionText}>{record.description}
-                                            </Text>
-                                            
-                                            
-                                            <Text style = {styles.datetimeText}>{record.datetime}
-                                            </Text>
-                                            
-                                    
-                                     
-                                            <View style = {styles.price}>
-                                            <Text style = {styles.text}>{record.price.currency}{record.price.value}
-                                           
-                                            
-                                     </Text>
-                                            </View>
-                                     </View>
-                                            );
-                                     }
-                                     });
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
- flexDirection: 'row',
-                                 
-//    justifyContent: 'center',
-//    alignItems: 'center',
-    //backgroundColor: 'black',
-  },
-                                 row: {
-                                 //flexDirection: 'row',
-                                // justifyContent: 'center',
-                                 padding: 10,
-                                 height:70,
-                                 backgroundColor: 'black',
+                                 container: {
+                                 flex: 1,
                                  },
-                                 
-                                 descriptionText : {
-                                 color: 'black',
-                                 //backgroundColor:'red',
-                                 margin:20,
-                                 marginTop:-50,
-                                 width:120,
-                                 height:20,
-                                 
-                                 
-                                 fontSize: 15,
-                                 fontWeight: 'bold',
-                                 
-                                 },
-                                 
-                                 descriptionBlock:{
-                                 
-                                 width:100,
-                                 height:50,
-                                 
-                                 
-                                
-                                
-                                 
-                                 },
-                                 
-                                 
-                                 datetimeText : {
-                                 color: 'black',
-                                 
-                                 fontSize: 10,
-                                 margin:20,
-                                 marginTop:-20,
-                                 },
-                                 
-                                 separator: {
-                                 height: 1,
-                                 backgroundColor: '#CCCCCC',
-                                 
-                                 },
-                                 
-                                 leftColor : {
-                                 backgroundColor:'orange',
-                                 width:15,
-                                 height: 70,
-                                 position: 'relative',
+                                 });
 
-                                 },
-                                 
-                                 price: {
 
-                                 width:75,
-                                 height:70,
-                                 backgroundColor: '#CCCCCC',
-                                 //marginLeft:250,
-                                 marginTop:-71,
-                                 marginRight: 30,
-                                 right:-260,
-                                
-
-                                 },
-                                 
-                                
-                                 text: {
-                                 fontSize: 17,
-                                 fontWeight: 'bold',
-                                 marginHorizontal:5,
-                                 marginVertical:25,
-                                 
-                                 
-                                 },
-                                 
-                                 
-                                 ListView: {
-                                 padding: 20,
-                                 
-                                 backgroundColor: '#F6F6F6',
-
-                                 },
-
-   
-});
-
-class NotitieBlok extends Component {
-  render() {
-    return (
-      <NavigatorIOS
-				style={styles.container}
-        initialRoute={{
-          component: Main,
-          title: 'Boekingen',
-					leftButtonIcon: require('image!NavBarButtonIcon'),
-					rightButtonIcon: require('image!NavBarButtonPlus'),
-					onLeftButtonPress: () => {console.log('pressed')},
-        }}
-        itemWrapperStyle={styles.itemWrapper}
-      />
-    );
-  }
+class Add extends Component {
+    render() {
+        return (
+                <ScrollView style={styles.list}>
+                <Text style={styles.welcome}>
+                Add something here
+                </Text>
+                </ScrollView>
+                );
+    }
 }
 
+class NotitieBlok extends Component {
+    render() {
+        return (
+                <NavigatorIOS
+                style={styles.container}
+                initialRoute={{
+                component: iOSPlaceholderView,
+                title: 'Boekingen',
+                leftButtonIcon: require('image!NavBarButtonIcon'),
+                rightButtonIcon: require('image!NavBarButtonPlus'),
+                onLeftButtonPress: () => {console.log('pressed')},
+                }}
+                itemWrapperStyle={styles.itemWrapper}
+                />
+                );
+    }
+}
 
 AppRegistry.registerComponent('NotitieBlok', () => NotitieBlok);
