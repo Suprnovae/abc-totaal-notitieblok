463731bef4efb69ba3b332cc0b882f5dc38ba169
islam drissi <zirconias777@yahoo.com>
2016-05-07 15:32:23 +0100

    Setup Camera extension #2
    Setup Camera extension for ios

---
+++ components/CameraIOSView.js
@@ -0,0 +1,98 @@
+import React, {
+  Component,
+  StyleSheet,Dimensions,
+  Text,
+  TouchableHighlight,
+  View
+} from 'react-native';
+import Camera from 'react-native-camera';
+
+class CameraIOSView extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      cameraType: Camera.constants.Type.back
+    };
+  }
+
+  render(){
+    return (
+      <View style={styles.container}>
+        <Camera
+          ref={(cam) => {
+            this.camera = cam;
+          }}
+          style={styles.preview}
+          type={this.state.cameraType}
+          torchMode={Camera.constants.FlashMode.off}
+          aspect={Camera.constants.Aspect.fill}>
+            <View style={styles.buttonBar}>
+                <TouchableHighlight style={styles.button} onPress={this._switchCamera.bind(this)}>
+                    <Text style={styles.buttonText}>Flip</Text>
+                </TouchableHighlight>
+                <TouchableHighlight style={styles.button} onPress={this._takePicture.bind(this)}>
+                    <Text style={styles.buttonText}>Take</Text>
+                </TouchableHighlight>
+            </View>
+
+        </Camera>
+      </View>
+    );
+  }
+
+  _switchCamera() {
+      console.log('switching camera');
+      var state = this.state;
+      state.cameraType = state.cameraType === Camera.constants.Type.back ? Camera.constants.Type.front : Camera.constants.Type.back;
+      this.setState(state);
+    }
+
+    _takePicture() {
+      console.log('taking picture');
+      this.camera.capture()
+        .then((data)=>console.log(data))
+        .catch(err => console.log(err));
+    }
+
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    height: Dimensions.get('window').height,
+    width: Dimensions.get('window').width
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    color: '#000',
+    padding: 10,
+    margin: 40
+  },
+    buttonBar: {
+        flexDirection: "row",
+        position: "absolute",
+        bottom: 25,
+        right: 0,
+        left: 0,
+        justifyContent: "center"
+    },
+    button: {
+        padding: 10,
+        borderWidth: 1,
+        borderColor: "#FFFFFF",
+        margin: 5
+    },
+    buttonText: {
+        color: "#FFFFFF"
+    }
+});
+
+module.exports = CameraIOSView;
--- index.ios.js
+++ index.ios.js
@@ -5,78 +5,93 @@
  */
 
 import React, {
-  AppRegistry,
-  Component,
-  NavigatorIOS,
-  StyleSheet,
-  Text,
-	ListView,
-  ScrollView,
-  View
+    AppRegistry,
+    Component,
+    NavigatorIOS,
+    StyleSheet,
+    Text,
+    ListView,
+    ScrollView,
+    View
 } from 'react-native';
 
 import DemoSimpleList from './components/DemoSimpleList';
+import CameraIOSView from './components/CameraIOSView';
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
+    container: {
+        flex: 1,
+    },
 });
 
 class Main extends Component {
-	constructor(props) {
-		super(props)
-    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-		this.state = {
-			dataSource: ds.cloneWithRows(['morgan', 'barclays', 'rabo', 'roth', 'rocker', 'london', 'new york', 'carnegie', 'lehman', 'v/d bilt', 'chase', 'hartman', 'stilman'])
-		}
-		console.log(props)
-		/*
-		props.navigator.push({
-          component: Add,
-          title: 'Nieuwe Boeking',
-					leftButtonTitle: 'Overzicht',
-					onLeftButtonPress: () => {props.navigator.pop()}
-    })
-*/
-	}
-  render() {
-    return (
-      <ScrollView style={styles.list}>
-				<DemoSimpleList dataSource={this.state.dataSource} />
-      </ScrollView>
-    );
-  }
+    constructor(props) {
+        super(props)
+        const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+        this.state = {
+            dataSource: ds.cloneWithRows(['morgan', 'barclays', 'rabo', 'roth', 'rocker', 'london', 'new york', 'carnegie', 'lehman', 'v/d bilt', 'chase', 'hartman', 'stilman'])
+        }
+        console.log(props)
+        /*
+         props.navigator.push({
+         component: Add,
+         title: 'Nieuwe Boeking',
+         leftButtonTitle: 'Overzicht',
+         onLeftButtonPress: () => {props.navigator.pop()}
+         })
+         */
+    }
+
+    render() {
+        return (
+            <ScrollView style={styles.list}>
+                <DemoSimpleList dataSource={this.state.dataSource}/>
+            </ScrollView>
+        );
+    }
 }
 
 class Add extends Component {
-  render() {
-    return (
-      <ScrollView style={styles.list}>
-        <Text style={styles.welcome}>
-          Add something here
-        </Text>
-      </ScrollView>
-    );
-  }
+    render() {
+        return (
+            <ScrollView style={styles.list}>
+                <Text style={styles.welcome}>
+                    Add something here
+                </Text>
+            </ScrollView>
+        );
+    }
 }
 
 class NotitieBlok extends Component {
-  render() {
-    return (
-      <NavigatorIOS
-				style={styles.container}
-        initialRoute={{
+    rightButtonPress() {
+        this.refs.nav.navigator.push({
+            title: "Camera",
+            component: CameraIOSView,
+            rightButtonTitle: 'Cancel',
+            onRightButtonPress: () => {
+                this.refs.nav.navigator.pop();
+            }
+        });
+    }
+
+    render() {
+        return (
+            <NavigatorIOS
+                ref="nav"
+                style={styles.container}
+                initialRoute={{
           component: Main,
           title: 'Boekingen',
 					leftButtonIcon: require('image!NavBarButtonIcon'),
 					rightButtonIcon: require('image!NavBarButtonPlus'),
 					onLeftButtonPress: () => {console.log('pressed')},
+          onRightButtonPress:this.rightButtonPress.bind(this)
         }}
-        itemWrapperStyle={styles.itemWrapper}
-      />
-    );
-  }
+                itemWrapperStyle={styles.itemWrapper}
+            />
+        );
+    }
 }
 
 AppRegistry.registerComponent('NotitieBlok', () => NotitieBlok);
