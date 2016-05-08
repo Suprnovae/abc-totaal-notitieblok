13dd59afb99c6d15ccd21dab39dcfea41f3eb352
islam drissi <zirconias777@yahoo.com>
2016-05-08 15:51:54 +0100

    adding camera support for android  issue #2
    this is a work in progress,
    added the camera support(library to android project, created a view for the camera)
    still need a navigator to push new views (from actionButton)

--- index.android.js
+++ index.android.js
@@ -13,6 +13,8 @@ import React, {
 import ActionButton from 'react-native-action-button';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
+import CameraANDROIDView from './components/CameraANDROIDView';
+
 import PlaceholderView from './components/PlaceholderView';
 import styles from './styles/Initial';
 const actions = [
@@ -21,24 +23,41 @@ const actions = [
 ];
 
 class NotitieBlok extends Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
     return (
-        <View style={{flex: 1}}>
-          <ToolbarAndroid
-            style={styles.toolbar}
-            icon={require('image!toolbar_icon')}
-            navIcon={require('image!toolbar_icon')}
-            title='ABC Notitie Blok'
-            actions={[]} />
-          <PlaceholderView />
-        <ActionButton buttonColor="#42A5F5">
-          <ActionButton.Item buttonColor="#FFC107" title="New">
-            <Icon name="rocket" size={30} style={{fontSize: 20, height: 22, color: 'white',}} />
-          </ActionButton.Item>
-        </ActionButton>
-        </View>
+      <View style={{flex: 1}}>
+        <ToolbarAndroid
+          style={styles.toolbar}
+          icon={require('image!toolbar_icon')}
+          navIcon={require('image!toolbar_icon')}
+          title='ABC Notitie Blok'
+          actions={[]} />
+        <PlaceholderView />
+      <ActionButton buttonColor="#42A5F5">
+        <ActionButton.Item buttonColor="#FFC107" title="New">
+          <Icon name="rocket" size={30} style={{fontSize: 20, height: 22, color: 'white',}} />
+        </ActionButton.Item>
+        <ActionButton.Item buttonColor='#1abc9c' title="camera" onPress={this._openCamera.bind(this)}>
+          <Icon name="camera" size={30} style={{fontSize: 20, height: 22, color: 'white',}} />
+        </ActionButton.Item>
+      </ActionButton>
+      </View>
     );
   }
+
+  _openCamera(){
+    //ToastAndroid.show('This is a toast with long duration', ToastAndroid.LONG);
+    console.log(this);
+    // this.refs.navigator.push({
+    //   title:'camera',
+    //   component:CameraANDROIDView,
+    // });
+    //return ( <CameraANDROIDView></CameraANDROIDView>);
+  }
+
 }
 
 AppRegistry.registerComponent('NotitieBlok', () => NotitieBlok);
