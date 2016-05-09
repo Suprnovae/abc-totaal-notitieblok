8d1010c81e0ef0a5b6a33a2a3b04bbf46b9efe69
David Asabina <david@supr.nu>
2016-05-09 07:33:39 -0400

    Clean-up iOSPlaceholderView

--- components/iOSPlaceholderView.js
+++ components/iOSPlaceholderView.js
@@ -3,13 +3,11 @@ import React, {
   View,
   Image,
   ListView,
-	ScrollView,
+  ScrollView,
   Text,
 } from 'react-native';
 
-
 import ListElementView from './ListElementView';
-import DemoCardList from './DemoCardList';
 import DemoSimpleList from './DemoSimpleList';
 
 import styles from '../styles/Initial';
@@ -17,26 +15,18 @@ import styles from '../styles/Initial';
 export default class iOSPlaceholderView extends Component {
 
   constructor(props) {
-        super(props)
-        const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-        this.state = {
-        dataSource: ds.cloneWithRows(['morgan', 'barclays', 'rabo', 'roth', 'rocker', 'london', 'new york', 'carnegie', 'lehman', 'v/d bilt', 'chase', 'hartman', 'stilman'])
-        }
-        console.log(props)
-        /*
-         props.navigator.push({
-         component: Add,
-         title: 'Nieuwe Boeking',
-         leftButtonTitle: 'Overzicht',
-         onLeftButtonPress: () => {props.navigator.pop()}
-         })
-         */
-    }
-    render() {
-        return (
-                <ScrollView style={styles.list}>
-                <DemoSimpleList dataSource={this.state.dataSource} />
-                </ScrollView>
-                );
-    }
+    super(props);
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      dataSource: ds.cloneWithRows(['morgan', 'barclays', 'rabo', 'roth', 'rocker', 'london', 'new york', 'carnegie', 'lehman', 'v/d bilt', 'chase', 'hartman', 'stilman'])
+    };
   }
+
+  render() {
+    return (
+      <ScrollView style={styles.list}>
+        <DemoSimpleList dataSource={this.state.dataSource} />
+      </ScrollView>
+    );
+  }
+}
